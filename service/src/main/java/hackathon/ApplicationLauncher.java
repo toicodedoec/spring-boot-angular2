119d2edaea07352f3a5357b89427dcbab3e6a9d6
doannx <doannx@gmail.com>
2016-10-25 22:57:10 +0700

    Create profiles in pom: dev (jar) & release (war)

--- service/src/main/java/hackathon/ApplicationLauncher.java
+++ service/src/main/java/hackathon/ApplicationLauncher.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
-//@SpringBootApplication
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
