235a439b31e05bcbb598f378bfc8fd7cedd1f66a
doannx <doannx@gmail.com>
2017-06-17 16:38:42 +0700

    Create new Angular2 app by angular-cli

---
+++ ui/src/main/fronend/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { HackathonPage } from './app.po';
+
+describe('hackathon App', () => {
+  let page: HackathonPage;
+
+  beforeEach(() => {
+    page = new HackathonPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
