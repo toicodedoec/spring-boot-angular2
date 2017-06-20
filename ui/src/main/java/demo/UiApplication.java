74493f99ef0978407c45d96cd371a308c4df01b0
doannx <doannx@gmail.com>
2017-06-20 09:03:52 +0700

    Refactor ui

--- ui/src/main/java/demo/UiApplication.java
+++ ui/src/main/java/demo/UiApplication.java
@@ -1,9 +1,13 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class UiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
